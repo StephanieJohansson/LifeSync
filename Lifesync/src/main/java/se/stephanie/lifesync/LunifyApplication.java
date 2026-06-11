02e9566bbf331395cf52dc7bd631820c75237893
Stephanie Johansson <3tomu24.johast@folkuniversitetet.nu>
2026-06-11 15:58:43 +0200

    refactor: rename project from LifeSync to Lunify

--- Lifesync/src/main/java/se/stephanie/lifesync/LunifyApplication.java
+++ Lifesync/src/main/java/se/stephanie/lifesync/LunifyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LifesyncApplication {
+public class LunifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LifesyncApplication.class, args);
+		SpringApplication.run(LunifyApplication.class, args);
 	}
 
 }
